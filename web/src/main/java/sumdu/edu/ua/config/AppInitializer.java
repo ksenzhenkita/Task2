2f8c8575e4a6f04dd5254b2eb5a783aebc2b9ee4
ksenzhenkita <tetiana.ksenzhenko@gmail.com>
2026-01-08 21:16:50 +0100

    Fix: remove manual logging versions to resolve Logback conflict

--- web/src/main/java/sumdu/edu/ua/config/AppInitializer.java
+++ web/src/main/java/sumdu/edu/ua/config/AppInitializer.java
@@ -2,12 +2,6 @@ package sumdu.edu.ua.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import jakarta.servlet.ServletContextEvent;
-import jakarta.servlet.ServletContextListener;
-import jakarta.servlet.annotation.WebListener;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import sumdu.edu.ua.repository.DbConfig;
 
 @SpringBootApplication(scanBasePackages = "sumdu.edu.ua")
 public class AppInitializer {
